b0039aca09e79e344a9af02a77203342d6a46563
zaldo232 <zaldo232@naver.com>
2025-06-28 11:21:34 +0900

    카테고리뷰에도 이미지 추가

--- KioskApp/Models/Menu.cs
+++ KioskApp/Models/Menu.cs
@@ -1,4 +1,7 @@
-﻿namespace KioskApp.Models
+﻿using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace KioskApp.Models
 {
     public class Menu
     {
@@ -8,5 +11,32 @@
         public string Description { get; set; }
         public int Price { get; set; }
         public string ImagePath { get; set; }
+
+        // 썸네일 이미지
+        public BitmapImage FileImage
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(ImagePath) || !File.Exists(ImagePath))
+                    return null;
+                try
+                {
+                    var bmp = new BitmapImage();
+                    using (var fs = new FileStream(ImagePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        bmp.BeginInit();
+                        bmp.CacheOption = BitmapCacheOption.OnLoad;
+                        bmp.StreamSource = fs;
+                        bmp.EndInit();
+                        bmp.Freeze();
+                    }
+                    return bmp;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
--- KioskApp/Models/MenuOptionValue.cs
+++ KioskApp/Models/MenuOptionValue.cs
@@ -1,4 +1,7 @@
-﻿namespace KioskApp.Models
+﻿using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace KioskApp.Models
 {
     public class MenuOptionValue
     {
@@ -7,5 +10,32 @@
         public string ValueLabel { get; set; }
         public int ExtraPrice { get; set; }
         public string ImagePath { get; set; }
+
+        // 썸네일 이미지
+        public BitmapImage FileImage
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(ImagePath) || !File.Exists(ImagePath))
+                    return null;
+                try
+                {
+                    var bmp = new BitmapImage();
+                    using (var fs = new FileStream(ImagePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        bmp.BeginInit();
+                        bmp.CacheOption = BitmapCacheOption.OnLoad;
+                        bmp.StreamSource = fs;
+                        bmp.EndInit();
+                        bmp.Freeze();
+                    }
+                    return bmp;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
